2757ef21904b2373ecb348acc1f5ee893a1bad7a
Ahmed Elshayeb <45132687+skyliquid22@users.noreply.github.com>
2020-08-29 13:08:11 -0700

    Added new methods to Connector
    - added make_request
    - added send_command
    - added get_orders
    - added get_rates
    - added other private functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import pandas as pd
-
-# module for working with time zone
-import pytz
+import datetime
 
 import MetaTrader5 as mt5
 
@@ -34,6 +32,7 @@ class Connector:
             self.account_info = mt5.account_info()._asdict()
             # self.symbols = mt5.symbols_get(group="*,!*USD*,!*EUR*,!*JPY*,!*GBP*")
             self.all_symbols = mt5.symbols_total()
+            self.currency = mt5.account_info().currency
             # Symbols in the Market watch window
             self.selected = []
 
@@ -61,15 +60,128 @@ class Connector:
         else:
             print('[symbol_info] Unable to get info from unselected symbols')
 
-    def get_rates(self, symbol, start, timeframe='TIMEFRAME_D1', count=270):
+    def make_request(self, action=mt5.TRADE_ACTION_DEAL, symbol='EURUSD', volume=1.0,
+                     type=mt5.ORDER_TYPE_BUY, market=False, sl_points=100, tp_points=100,
+                     magic=123456, comment='MT5 Python'):
+        """
+        :param action: (int) trade action. example: mt5.TRADE_ACTION_DEAL
+        :param symbol: (str) symbol
+        :param volume: (float) trade volume
+        :param type: (int) trade type. example: mt5.ORDER_TYPE_BUY
+        :param market: (bool) True for market order. Default is False
+        :param sl_points: (int) n# of point below ask price to close trade
+        :param tp_points: (int) n# of point above ask price to close trade
+        :param magic: (int) the magic number :)
+        :param comment: (str) comment
+
+        """
+        symbol_info = mt5.symbol_info(symbol)
+        if symbol_info is None:
+            print(symbol, "not found, can not call order_check()")
+
+        # if the symbol is unavailable in MarketWatch, add it
+        if not symbol_info.visible:
+            print(symbol, "is not visible, trying to switch on")
+            if not mt5.symbol_select(symbol, True):
+                print("symbol_select({}}) failed, exit", symbol)
+
+        # prepare the request
+        point = mt5.symbol_info(symbol).point
+        request = {
+            "action": action,
+            "symbol": symbol,
+            "volume": volume,
+            "type": type,
+            "price": mt5.symbol_info_tick(symbol).ask,
+            "sl": mt5.symbol_info_tick(symbol).ask - sl_points * point,
+            "tp": mt5.symbol_info_tick(symbol).ask + tp_points * point,
+            "deviation": 10,
+            "magic": magic,
+            "comment": comment,
+            "type_time": mt5.ORDER_TIME_GTC,  # The order stays in the queue until it is manually canceled
+            "type_filling": mt5.ORDER_FILLING_RETURN,  # https://www.mql5.com/en/docs/integration/python_metatrader5/mt5ordercheck_py
+        }
+        if market:
+            request['price'] = 0.0
+            request['type_filling'] = mt5.ORDER_FILLING_IOC
+
+        return mt5.order_check(request)
+
+    def send_command(self, request):
+        """
+
+        """
+        result = mt5.order_send(request)
+        # check the execution result
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("send_command, ret code={}".format(result.retcode))
+            # request the result as a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if this is a trading request structure, display it element by element as well
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+            print("shutdown() and quit")
+            mt5.shutdown()
+            quit()
+
+        print("send_command done, ", result)
+
+    @staticmethod
+    def get_orders_count():
+        return mt5.orders_total()
+
+    @staticmethod
+    def get_orders(**kwargs):
+        """
+        symbol="Symbol"
+        group="Group" i.e. group="*GBP*" > get the list of orders on symbols whose names contain "*GBP*"
+        ticket="Ticket"
+        """
+        orders = mt5.orders_get(**kwargs)
+        if orders is None:
+            print("No orders found, error code={}".format(mt5.last_error()))
+            return pd.DataFrame()
+        else:
+            df = pd.DataFrame(list(orders), columns=orders[0]._asdict().keys())
+            df['time_setup'] = pd.to_datetime(df['time_setup'], unit='s')
+            return df
+
+    def get_rates(self, symbol, start=0, timeframe='TIMEFRAME_D1', count=270, end=None):
         """
 
         :param symbol: (str)
         :param timeframe: (str) the time interval, TIMEFRAME_M1: 1min, _H, _D, _W, _MN
-        :param start: (date or int) the start of the data, can be date or bar index
+        :param start: (str or int) the start of the data, can be date or bar index
         :param count: (int) number of bars to return
+        :param end: (str) (optional) sets the end date instead of using count
         :return: (DateFrame)
         """
+        # check if start is a bar index
+        if isinstance(start, int):
+            rates = pd.DataFrame(mt5.copy_rates_from_pos(symbol, timeframe, start, count))
+            rates['time'] = pd.to_datetime(rates['time'], unit='s')
+            return rates
+        else:
+            try:
+                start_time = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S UTC')
+                # create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
+                start_time.replace(tzinfo=datetime.timezone.utc)
+                if end is not None:
+                    end_time = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S UTC')
+                    end_time.replace(tzinfo=datetime.timezone.utc)
+                    rates = pd.DataFrame(mt5.copy_rates_range(symbol, timeframe, start_time, end_time))
+                    rates['time'] = pd.to_datetime(rates['time'], unit='s')
+                    return rates
+                else:
+                    rates = pd.DataFrame(mt5.copy_rates_from(symbol, timeframe, start_time, count))
+                    rates['time'] = pd.to_datetime(rates['time'], unit='s')
+                    return rates
+            except TypeError:
+                print('[get_rates] start type neither date nor bar index')
 
     @staticmethod
     def tick_info(symbols):
@@ -84,6 +196,20 @@ class Connector:
             if tick_info is not None:
                 tick_info_list.append(tick_info)
         return pd.concat(tick_info_list)
+
+    @staticmethod
+    def _get_open_trades_count():
+        """
+        Returns the number of open positions
+        """
+        return mt5.positions_total()
+
+    @staticmethod
+    def _get_open_trades(**kwargs):
+        """
+        TODO : https://www.mql5.com/en/docs/integration/python_metatrader5/mt5positionsget_py
+        """
+        return mt5.positions_get(**kwargs)
 
     def _select_symbol(self, symbols):
         """
